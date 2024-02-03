1407a57a8537485ccda58ae5ead34a7726a1b748
aygaoua <102914808+aygaoua@users.noreply.github.com>
2024-02-03 19:02:35 +0100

    make the unic test for FragTrap

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,22 +6,44 @@
 /*   By: azgaoua <azgaoua@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 17:28:12 by azgaoua           #+#    #+#             */
-/*   Updated: 2024/02/03 17:58:21 by azgaoua          ###   ########.fr       */
+/*   Updated: 2024/02/03 18:55:26 by azgaoua          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main() {
-	ScavTrap a(std::string("Aymane"));
+	std::cout << "<<------------------- ClapTrap ------------------->>" << std::endl;
+	ClapTrap a(std::string("Aymane"));
+	
+	a.attack(std::string("salmane"));
+	a.takeDamage(5);
+	a.beRepaired(5);
 
-	a.attack(std::string("salmane"));
-	a.takeDamage(500);
-	a.attack(std::string("salmane"));
-	a.guardGate();
-	a.attack(std::string("salmane"));
-	a.beRepaired(101);
-	a.guardGate();
+	std::cout << "<<------------------- ScavTrap ------------------->>" << std::endl;
+	ScavTrap b(std::string("Aymane"));
+
+	b.attack(std::string("salmane"));
+	b.takeDamage(500);
+	b.attack(std::string("salmane"));
+	b.guardGate();
+	b.attack(std::string("salmane"));
+	b.beRepaired(101);
+	b.guardGate();
+
+	std::cout << "<<------------------- FragTrap ------------------->>" << std::endl;
+	FragTrap c(std::string("Aymane"));
+	
+	c.attack(std::string("salmane"));
+	c.takeDamage(500);
+	c.attack(std::string("salmane"));
+	c.highFivesGuys();
+	c.attack(std::string("salmane"));
+	c.beRepaired(101);
+	c.highFivesGuys();
+	std::cout << "<<------------------- End ------------------->>" << std::endl;
 
 	return (0);
 }
