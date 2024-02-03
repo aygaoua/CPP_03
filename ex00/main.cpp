8c0c846e56fbd9ac5e986a4a3b52bbf3732ff36f
aygaoua <102914808+aygaoua@users.noreply.github.com>
2024-02-03 18:24:20 +0100

    cleaning the main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,18 +6,20 @@
 /*   By: azgaoua <azgaoua@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 17:28:12 by azgaoua           #+#    #+#             */
-/*   Updated: 2024/02/02 18:09:04 by azgaoua          ###   ########.fr       */
+/*   Updated: 2024/02/03 18:23:34 by azgaoua          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 int main() {
+	std::cout << "<<------------------- ClapTrap ------------------->>" << std::endl;
 	ClapTrap a(std::string("Aymane"));
 	
 	a.attack(std::string("salmane"));
 	a.takeDamage(5);
 	a.beRepaired(5);
+	std::cout << "<<------------------- End ------------------->>" << std::endl;
 	return (0);
 
 }
